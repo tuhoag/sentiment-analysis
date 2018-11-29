7bca5e3a3a8494f8eb6bda4793b79e0cc795bf25
Tu Hoang <hatu87@gmail.com>
2018-11-29 12:42:55 +0100

    add some plot funcs

--- explore_data.py
+++ explore_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
 
 def get_num_classes(labels):
     """Get total number of classes.
@@ -24,6 +25,12 @@ def get_num_classes(labels):
     return num_classes
 
 def plot_class_distribution(labels):
+    """Plot class distribution.
+
+    Arguments:
+        labels {list} -- list of labels.
+    """
+
     num_classes = get_num_classes(labels)
 
     counter_map = Counter(labels)
@@ -43,17 +50,66 @@ def get_num_words_per_sample(sample_texts):
     word_lens = []
 
     for text in sample_texts:
-        word_lens.append(len(text))
+        word_lens.append(len(text.split()))
 
     return np.median(word_lens)
 
 def plot_frequency_distribution_of_ngrams(sample_texts,
                                           ngram_range=(1, 2),
                                           num_ngrams=50):
-    pass
+    """Plot frequency of all ngrams.
+
+    Arguments:
+        sample_texts {list} -- list of texts.
+
+    Keyword Arguments:
+        ngram_range {tuple} -- range of ngram size to plot (default: {(1, 2)})
+        num_ngrams {int} -- number of top frequent ngram to plot (default: {50})
+    """
+
+    kwargs = {
+        'ngram_range': ngram_range,
+        'analyzer': 'word',
+        'strip_accents': 'unicode',
+        'decode_error': 'replace',
+        'dtype': 'int32'
+    }
+
+    vectorizer = CountVectorizer(**kwargs)
+    vectorizer.fit(sample_texts)
+
+    vectorized_texts = vectorizer.transform(sample_texts)
+
+    all_ngrams = list(vectorizer.get_feature_names())
+    num_ngrams = min(num_ngrams, len(all_ngrams))
+
+    all_counts = vectorized_texts.sum(axis=0).tolist()[0]
+
+    all_counts, all_ngrams = zip(*[(c, n) for c, n in sorted(zip(all_counts, all_ngrams), key=lambda t: t[0], reverse=True)])
+    plot_counts = all_counts[:num_ngrams]
+    plot_ngrams = all_ngrams[:num_ngrams]
+
+    idx = np.arange(num_ngrams)
+    plt.bar(idx, plot_counts, width=0.6)
+    plt.xlabel('N-grams')
+    plt.ylabel('Frequencies')
+    plt.title('Frequency distribution of n-grams')
+    plt.xticks(idx, plot_ngrams, rotation=60)
+    plt.show()
 
 def plot_sample_length_distribution(sample_texts):
-    pass
+    """Plot text length distribution.
+
+    Arguments:
+        sample_texts {list} -- list of texts.
+    """
+
+    all_counts = [len(s) for s in sample_texts]
+    plt.hist(all_counts, 50)
+    plt.xlabel('Length of a sample')
+    plt.ylabel('Number of samples')
+    plt.title('Sample length distribution')
+    plt.show()
 
 def main():
     plot_class_distribution([0, 1])
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from load_data import load_imdb_sentiment_analysis_dataset
-from explore_data import plot_class_distribution
+from explore_data import plot_class_distribution, plot_frequency_distribution_of_ngrams, plot_sample_length_distribution
 
 def main():
     train_data, test_data = load_imdb_sentiment_analysis_dataset('./data/aclImdb')
 
-    plot_class_distribution(train_data[1])
+    # plot_class_distribution(train_data[1])
+    plot_frequency_distribution_of_ngrams(train_data[0])
+    plot_sample_length_distribution(train_data[0])
 
 if __name__ == "__main__":
     main()
